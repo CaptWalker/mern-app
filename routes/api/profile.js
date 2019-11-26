810a7dd3d4602753443e0433ab22114c74e3dbee
dhruv <dhruv@LAPTOP-Q90EM56C>
2019-11-26 18:50:17 +0530

    getting repo from github

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const {check, validationResult} = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 
 //@route    GET api/profile/me
@@ -308,5 +310,34 @@ try {
     return res.status(500).send('Server Error');
 }
 });
+
+//@route    GET api/profile/github/:username
+//@desc     get user repo from gtihub
+//@access   public
+
+router.get('/github/:username', (req, res) =>{
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers:{'user-agent':'node.js'}
+
+        };
+
+        request(options, (error, response, body)=>{
+            if(error) console.error(error.message);
+
+            if(response.statusCode !== 200){
+                return res.status(400).json({msg:'No github profile found'})
+            }
+
+            res.json(JSON.parse(body));
+        });
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
